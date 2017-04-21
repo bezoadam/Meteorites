b05689e5b813f069035d85795151f8b5adf2dd98
Adam Bezak <bezoadam95@gmail.com>
2017-04-21 23:08:18 +0200

    sorted array

---
+++ Meteorites/MeteorCell.swift
@@ -0,0 +1,21 @@
+//
+//  MeteorCell.swift
+//  Meteorites
+//
+//  Created by Adam Bezák on 21.4.17.
+//  Copyright © 2017 Adam Bezák. All rights reserved.
+//
+
+import UIKit
+
+class MeteorCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
--- Meteorites/ViewController.swift
+++ Meteorites/ViewController.swift
@@ -13,19 +13,25 @@ import RealmSwift
 
 class ViewController: UITableViewController {
     
-    var meteorites : [Meteor]?
+    var meteorites : [Meteor]!
+    let cellId = "cellId"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.title = "Meteorites"
         let manager = DataManager()
         let results = manager.objects(type: Meteor.self)
-        meteorites = Array(results!)
-        manager.deleteAll()
+//        .sort(by: { $0.meetingDate.compare($1.meetingDate) == .orderedAscending})
+        meteorites = Array(results!).sorted(by: { $0.mass > $1.mass})
+//        manager.deleteAll()
+        
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+//        self.tableView.register(MeteorCell.self, forCellReuseIdentifier: cellId)
+        
         if meteorites?.count == 0 {
             Service.sharedInstance.fetchData(completion: { (meteorites) in
                 self.meteorites = meteorites
-                print(self.meteorites?.count)
             })
         }
     }
@@ -34,6 +40,34 @@ class ViewController: UITableViewController {
         super.didReceiveMemoryWarning()
     }
 
-
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        var cell : UITableViewCell? = tableView.dequeueReusableCell(withIdentifier: cellId) as? MeteorCell
+        if cell == nil {
+            cell = UITableViewCell(style: UITableViewCellStyle.subtitle, reuseIdentifier: cellId)
+        }
+        
+        cell?.textLabel?.text = meteorites?[indexPath.row].name
+        cell?.detailTextLabel?.text = meteorites?[indexPath.row].mass.stringValue
+        cell?.selectionStyle = .none
+        return cell!
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (meteorites?.count)!
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
 }
 
+extension Int {
+    var stringValue:String {
+        return "\(self)"
+    }
+}
