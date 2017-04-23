9f05bf3528354d24d516cba828b39ea42f6478c7
Adam Bezak <bezoadam95@gmail.com>
2017-04-23 23:49:20 +0200

    fixed bug in date actualization

--- Meteorites/ViewController.swift
+++ Meteorites/ViewController.swift
@@ -18,22 +18,21 @@ class ViewController: UITableViewController {
     let originalColor = DynamicColor(hexString: "#c0392b")
     let cellId = "cellId"
     let cellDetailId = "cellDetailId"
-    var detailedMeteorCell: UITableViewCell?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Meteorites"
         let manager = DataManager()
-//        smanager.deleteAll()
+//        manager.deleteAll()
         
         if let results = manager.objects(type: Meteor.self) {
             self.meteorites = Array(results).sorted(by: { $0.mass > $1.mass})
             if (self.meteorites?.count)! > 0 && InternetReachability.isConnectedToNetwork(){
                 let oneMeteorLastUpdateTime = Int32((self.meteorites?[0]?.lastUpdate.timeIntervalSinceNow)!)
                 if abs(oneMeteorLastUpdateTime) > 86400 {
-                    manager.deleteAll()
                     Service.sharedInstance.fetchData(completion: { (meteorites, err) in
                         let manager = DataManager()
+                        manager.deleteAll()
                         
                         for m in meteorites! {
                             manager.add(meteor: m)
@@ -84,6 +83,7 @@ class ViewController: UITableViewController {
             cellAudioButton.addTarget(self, action: #selector(handleShowDetail(sender:)), for: .touchUpInside)
             cellAudioButton.setImage(UIImage(named: "expand"), for: .normal)
             cellAudioButton.contentMode = .scaleAspectFit
+            
             cellAudioButton.tag = indexPath.row
             cell?.selectionStyle = .none
             cell?.accessoryView = cellAudioButton as UIView
@@ -172,6 +172,7 @@ class ViewController: UITableViewController {
     
     private func expandCell(tableView: UITableView, index: Int) {
         if let _ = meteorites?[index] {
+            
             meteorites?.insert(nil, at: index + 1)
             tableView.insertRows(at: [NSIndexPath(row: index + 1, section: 0) as IndexPath] , with: .top)
         }
